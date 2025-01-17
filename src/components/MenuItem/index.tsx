8b4955d412bb0626b3dd2d7073a5cf5a7e4f8a15
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-01-17 21:14:45 +0100

    🎨 Add click event to MenuItem

--- src/components/MenuItem/index.tsx
+++ src/components/MenuItem/index.tsx
@@ -1,19 +1,32 @@
-import { ReactNode } from "react";
+import { ReactNode, useCallback } from "react";
 
 import styles from "./styles.module.css";
 
 export interface MenuItemProps {
-  onClick?: () => void;
+  onClick?: (event: React.MouseEvent<HTMLElement>) => void;
   disabled?: boolean;
   className?: string;
   children: ReactNode;
 }
 
-const MenuItem = ({ children, className }: MenuItemProps) => {
+const MenuItem = ({
+  children,
+  onClick,
+  disabled,
+  className,
+}: MenuItemProps) => {
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLElement>) => {
+      if (onClick) onClick(event);
+    },
+    [onClick]
+  );
+
   return (
     <div
+      onClick={handleClick}
       className={`${styles.menuItem} ${className}`}
-      aria-disabled={false}
+      aria-disabled={disabled}
       role="menuitem"
       tabIndex={-1}
     >
