a32f634df403f1a5f68419307ee2c38a046ee6e5
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-01-19 13:01:41 +0100

    🎨 Add ContextMenu to App

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,4 +1,4 @@
-// import { ContextMenu } from "../src/components";
+import { ContextMenu } from '../../src/components';
 
 const App = () => (
   <>
@@ -6,12 +6,12 @@ const App = () => (
       <p id="context-menu-trigger">Right click to trigger</p>
     </div>
 
-    {/* <ContextMenu triggerId="context-menu-trigger">
+    <ContextMenu triggerId="context-menu-trigger">
       <ContextMenu.Item disabled>Item 1</ContextMenu.Item>
       <ContextMenu.Item>Item 2</ContextMenu.Item>
       <ContextMenu.Separator />
       <ContextMenu.Item>Item 3</ContextMenu.Item>
-    </ContextMenu> */}
+    </ContextMenu>
   </>
 );
 
