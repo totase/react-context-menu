d699e4d1631430b6ec0650b43bc13ce946b0ecd9
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-01-19 20:42:26 +0100

    🚚 Adjust export of components

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as ContextMenu } from './ContextMenu';
+export { default as MenuItem } from './MenuItem';
+export { default as Separator } from './Separator';
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import './styles.css';
 
-export { default as ContextMenu } from './components/ContextMenu';
-export { default as MenuItem } from './components/MenuItem';
-export { default as Separator } from './components/Separator';
+export { ContextMenu, MenuItem, Separator } from './components';
