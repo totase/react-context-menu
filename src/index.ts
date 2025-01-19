5dc9eb083871eae70be648628d65a031640efca5
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-01-19 20:41:25 +0100

    ♻️ Put styles in styles.css

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './styles.css';
+
 export { default as ContextMenu } from './components/ContextMenu';
 export { default as MenuItem } from './components/MenuItem';
 export { default as Separator } from './components/Separator';
