d1abd1cf757d3958917c675dbbc33e840e62718d
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-01-23 12:50:41 +0100

    🎨 Update app with sub menu

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -14,8 +14,13 @@ const App = () => {
       <ContextMenu triggerId="context-menu-trigger">
         <ContextMenu.Item disabled>Item 1</ContextMenu.Item>
         <ContextMenu.Item onClick={handleClick}>Item 2</ContextMenu.Item>
-        <ContextMenu.Separator />
         <ContextMenu.Item onClick={handleClick}>Item 3</ContextMenu.Item>
+        <ContextMenu.Separator />
+        <ContextMenu.SubMenu label="Sub menu">
+          <ContextMenu.Item onClick={handleClick}>Sub item 1</ContextMenu.Item>
+          <ContextMenu.Item onClick={handleClick}>Sub item 2</ContextMenu.Item>
+        </ContextMenu.SubMenu>
+        <ContextMenu.Item onClick={handleClick}>Sub item 2</ContextMenu.Item>
       </ContextMenu>
     </>
   );
