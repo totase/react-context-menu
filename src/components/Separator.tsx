041f602b05f64fba94abc99f66878353b5d903b9
Tommy Sevaldsen <tommysevaldsen@gmail.com>
2025-04-17 19:06:08 +0200

    fix(Separator): Add props + class names

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,3 +1,12 @@
-const Separator = () => <hr className="react-context-menu__separator" />;
+import { HTMLAttributes } from 'react';
+import cx from 'clsx';
+
+export interface SeparatorProps extends HTMLAttributes<HTMLHRElement> {}
+
+const Separator = ({ className, ...rest }: SeparatorProps) => {
+  const classNames = cx('react-context-menu__separator', className);
+
+  return <hr className={classNames} {...rest} />;
+};
 
 export default Separator;
